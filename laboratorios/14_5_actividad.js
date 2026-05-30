47b0f942268bcc413fd722d2193fe931cd631567
Julian Malpica <elkhanekas@gmail.com>
2026-05-29 23:01:11 -0500

    entregable 14_5_actividad constructores.

---
+++ laboratorios/14_5_actividad.js
@@ -0,0 +1,9 @@
+// Ejercicio 1: Libro (Objeto básico + if)
+// Crear un sistema de préstamo de libros.
+// Tareas:
+// 1.Usa el constructor Libro para crear un objeto con título y autor.
+// 2.Agrega una propiedad prestado = false.
+// 3.Implementa el método prestar() que:
+// •Cambie prestado a true si el libro está disponible.
+// •Muestre un mensaje de error si ya está prestado (usando if).
+// 4.Prueba prestar el libro dos veces y observa los mensajes.
